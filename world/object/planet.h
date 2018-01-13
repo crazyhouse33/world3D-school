6c58bd471b4594c6db80804c19309dd79c811bfd
huitredelombre <hugo.alonso@etu.u-bordeaux.fr>
2018-01-13 17:51:39 +0100

    Classe Camera, divers avancées

--- world/object/planet.h
+++ world/object/planet.h
@@ -1,24 +1,27 @@
 #ifndef PLANET
 #define PLANET
 #include "chainedTriangle.h"
+#include <forward_list>
 class Planet{
 	private:
-		ChainedTriangle* mesh;
+		ChainedTriangle** mesh; //the first level of precision
+		
 		/*basic data used for random generation*/
 		int humidity;
-		int temperatureAtEcuador;
+		double temperatureAtEcuador;
 		double* center; //(x, y, z)
 		/*for now we use fixed temp at ecuador, if we got enought
 		time we will generate smalls suns systems where temperature
 		is more realisticlely regulated 
-
 		Planet** suns;
 		*/
+	
+	public:
 		/*
 		 * Function returning the actual temperature of the planet 
-		 * on a given point*/
+		 * on a given point
+		 * */
 
-	public:
 		double temperatureAtVertex(double* Vertex);
 		double*getCenter();
 		void moveCenter(double* dxyz);
