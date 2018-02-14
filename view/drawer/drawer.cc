ac3600fa609d57efe17d09ae106dccda5bc92cd0
huitredelombre <hugo.alonso@etu.u-bordeaux.fr>
2018-02-14 19:50:13 +0100

    truc mineur pour pull

--- view/drawer/drawer.cc
+++ view/drawer/drawer.cc
@@ -9,9 +9,9 @@ void Drawer::draw(Triangle triangle){//TODO use VBO thanks to gpu class
 	glVertex3f(triangle.v1[0],triangle.v1[1], triangle.v1[2]);
 	glVertex3f(triangle.v2[0],triangle.v2[1], triangle.v2[2]);
 	glVertex3f(triangle.v3[0],triangle.v3[1], triangle.v3[2]);
-
-
 	glEnd();
-
-
 }
+/*
+void Drawer::draw(Planet planet){
+
+}*/
