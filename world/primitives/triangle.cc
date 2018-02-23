f32a4bf6fc01636877c18b6dfc470796a97f9a21
huitredelombre <hugo.alonso@etu.u-bordeaux.fr>
2018-02-23 18:31:33 +0100

    révision cassage de triangle

--- world/primitives/triangle.cc
+++ world/primitives/triangle.cc
@@ -15,11 +15,23 @@ float* Triangle::crack(float distance){
 	vertex[1]= (v3[1]+v2[1]+v1[1])/3;
 	vertex[2]= (v3[2]+v2[2]+v1[2])/3;
 
-	float norm = distance / sqrt( vertex[2] * vertex[2] + vertex[1] * vertex[1] + vertex[0] * vertex[0]);
+	//getting cross Product
 	
-	vertex[0]*=norm;
-	vertex[1]*=norm;
-	vertex[2]*=norm;
+	float crossX= v1[1] * v2[2] - v1[2]*v2[1];
+	float crossY= v1[2] * v2[0] - v1[0]*v2[2];
+	float crossZ= v1[0] * v2[1] - v1[1]*v2[0];
+
+
+	//norming it
+	float norm = distance / sqrt( crossZ * crossZ + crossY * crossY + crossX * crossX);
 	
+	crossX*=norm;
+	crossY*=norm;
+	crossZ*=norm;
+	
+	vertex[2]+=crossZ;
+	vertex[1]+=crossY;
+	vertex[0]+=crossX;
+
 	return vertex;
 }
